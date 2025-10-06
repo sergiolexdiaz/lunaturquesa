e0bb44a085ec34dad015121c064462067d951111
sergiolexdiaz <133723415+sergiolexdiaz@users.noreply.github.com>
2025-10-06 11:32:08 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function setLang(lang) {
-  document.querySelectorAll('.text').forEach(el => el.classList.add('hidden'));
-  document.querySelectorAll('.' + lang).forEach(el => el.classList.remove('hidden'));
-}
